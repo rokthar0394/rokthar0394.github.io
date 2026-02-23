366cda8e555ddd7fbd2548b15bd0d346204c6435
Ricardo Javier Esparza Torres <rokthar.code@gmail.com>
2026-02-22 22:29:08 -0500

    fix: configurar el build format para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rokthar0394.github.io/',
-  base: '/rokthar0394.github.io',
+  base: '/',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
